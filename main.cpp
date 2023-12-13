fbadce4c0adae0595993c0d7c758ce25901e0dcc
wangcheng <wangcheng@baltamatica.com>
2023-12-13 17:56:46 +0800

    测试Add单元

--- main.cpp
+++ main.cpp
@@ -1,29 +1,40 @@
 #include <memory>
+#include <utility>
 
+#include "Add.h"
 #include "Const.h"
 #include "Simodel.h"
-#include "typedefine.h"
 
 using namespace arma;
 
 int main() {
   auto const1 = std::make_shared<simodel::Const>();
-  const1->update();
-  const1->getOutput(0).print();
+  const1->setParameter("constant", mat(1, 1, fill::value(1)));
 
   auto const2 = std::make_shared<simodel::Const>();
-  const2->update();
-  const2->getOutput(0).print();
+  const2->setParameter("constant", mat(1, 1, fill::value(2)));
 
   auto const3 = std::make_shared<simodel::Const>();
-  const3->update();
-  const3->getOutput(0).print();
+  const3->setParameter("constant", mat(1, 1, fill::value(3)));
+
+  auto add = std::make_shared<simodel::Add>();
+  add->setParameter("operators", std::string("+-+"));
 
   auto sm = std::make_shared<simodel::Simodel>();
   sm->addUnit(const1);
   sm->addUnit(const2);
   sm->addUnit(const3);
+  sm->connectUnit(std::make_pair(0, 1), std::make_pair(3, 0));
+  sm->connectUnit(std::make_pair(1, 1), std::make_pair(3, 1));
+  sm->connectUnit(std::make_pair(2, 1), std::make_pair(3, 2));
 
-  sm->deleteUnit(2);
-  sm->deleteUnit(1);
+  const1->update();
+  const2->update();
+  const3->update();
+  add->setInput(0, const1->getOutput(0));
+  add->setInput(1, const2->getOutput(0));
+  add->setInput(2, const3->getOutput(0));
+  add->update();
+
+  add->getOutput(0).print();
 }
